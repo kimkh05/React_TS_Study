3212a77975bc8b204be5c1c147ae446019621f3a
kimkh05 <nestjs01@naver.com>
2022-04-07 23:25:45 +0900

    [ADD] GlobalStyle 설정

---
+++ test/src/style/globalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        box-sizing: border-box;
+        border: none;
+    }
+`;
