52d52dd072ed5963a49d0c9aad8ce3ccb7fa737e
kimkh05 <nestjs01@naver.com>
2022-04-11 22:57:51 +0900

    [ADD] 파일 추가

---
+++ test/src/components/list/list.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import * as S from "./styles";
+import { SearchData } from "../../constance/Search";
+
+const List: React.FC = ({props} : any) => {
+    console.log(props);
+  return (
+    <>
+      <S.Wrapper>
+        <S.Post>
+            
+        </S.Post>
+      </S.Wrapper>
+    </>
+  );
+};
+
+export default List;
---
+++ test/src/components/list/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 1680px;
+  height: 970px;
+  background: #ede7f6;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Post = styled.div`
+  margin-top: 30px;
+  width: 800px;
+  height: 900px;
+  background: #fff;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  p{
+      font-size: 48px;
+      font-weight: bold;
+      margin: 100px 0 0 100px;
+
+  }
+`;
