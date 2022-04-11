c7da280e50559234f221ed5f180f78b46c9dd8a3
kimkh05 <nestjs01@naver.com>
2022-04-11 23:17:08 +0900

    [MODIFY] 주석 처리

--- test/src/components/list/list.tsx
+++ test/src/components/list/list.tsx
@@ -2,13 +2,20 @@ import React, { useEffect } from "react";
 import * as S from "./styles";
 import { SearchData } from "../../constance/Search";
 
-const List: React.FC = ({props} : any) => {
-    console.log(props);
+const List: React.FC = ({ props }: any) => {
+  console.log(props);
   return (
     <>
       <S.Wrapper>
         <S.Post>
-            
+          {/* {SearchData.map((value, index) => {
+            return props.name === value.name ? (
+              <div key={index}>
+                <span>{value.name}</span>
+                <span>{value.date}</span>
+              </div>
+            ) : null;
+          })} */}
         </S.Post>
       </S.Wrapper>
     </>
