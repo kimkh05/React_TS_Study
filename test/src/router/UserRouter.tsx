db1ed0fb6182b90e9b73ff9fe2facb10188c77f9
kimkh05 <nestjs01@naver.com>
2022-04-11 12:16:50 +0900

    [ADD] Search 불러오기

--- test/src/router/UserRouter.tsx
+++ test/src/router/UserRouter.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Search from "../components/Search/search";
 
 const UserRouter: React.FC = () => {
-    
   return (
     <BrowserRouter>
-      <Route path="/" exact element={<Search />} />
-      <Route path="/${s}" element={<$sss />}
+      <Routes>
+        <Route path="/" element={<Search />} />
+        
+      </Routes>
     </BrowserRouter>
   );
 };
