5e4891ae46b63eefeccc7336c75ed270095ad6c1
kimkh05 <nestjs01@naver.com>
2022-04-13 22:29:13 +0900

    [MODIFY] 에러 페이지 추가

--- test/src/router/UserRouter.tsx
+++ test/src/router/UserRouter.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import List from "../components/list/list";
 import Search from "../components/Search/search";
+import ErrorPage from "../components/ErrorPage/errorPage";
 
 const UserRouter: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const UserRouter: React.FC = () => {
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/list/:name" element={<List />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
