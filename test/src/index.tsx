c4c037b1943bcd31b2e182b5d829394fb01c360c
kimkh05 <nestjs01@naver.com>
2022-04-14 14:46:37 +0900

    [MODIFY] 코드 복구

--- test/src/index.tsx
+++ test/src/index.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
